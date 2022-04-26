84c860067a9149671bd41e3c004d00b4724d43df
orozcoding <orozcoding@gmail.com>
2022-04-26 05:53:28 -0400

    Finish Users spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,7 +6,14 @@ RSpec.describe "Users", type: :request do
       get "/users/"
       expect(response).to have_http_status(:ok)
       expect(response).to render_template(:index)
-      expect(response.body).to include('Find me in app/views/users/index.html.erb')
+      expect(response.body).to include('This is a list of all users')
+    end
+
+    it "shows user info" do
+      get "/users/20"
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template(:show)
+      expect(response.body).to include('Showing User info')
     end
   end
 end
