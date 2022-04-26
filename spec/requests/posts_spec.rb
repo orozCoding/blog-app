50da09ba1442900265d7a1405ace7d45bc385e5b
orozcoding <orozcoding@gmail.com>
2022-04-26 05:55:58 -0400

    Create Posts Spec

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,19 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "Posts", type: :request do
-#   describe "GET /index" do
-#     pending "add some examples (or delete) #{__FILE__}"
-#   end
-# end
+RSpec.describe "Posts", type: :request do
+  describe "Using Posts controller" do
+    it "gets posts list from an user" do
+      get "/users/20/posts"
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template(:index)
+      expect(response.body).to include('Showing all the posts of the user')
+    end
+
+    it "Show post info" do
+      get "/users/20/posts/1"
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template(:show)
+      expect(response.body).to include('Showing specific post with the id')
+    end
+  end
+end
