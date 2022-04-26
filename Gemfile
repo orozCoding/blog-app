05eb38a0c051f47985faee6a8f1606a65289c9ac
orozcoding <orozcoding@gmail.com>
2022-04-26 06:03:30 -0400

    Fix linters

--- Gemfile
+++ Gemfile
@@ -7,8 +7,6 @@ gem 'rubocop', '>= 1.0', '< 2.0'
 
 gem 'ffi'
 
-gem 'capybara'
-
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.2', '>= 7.0.2.3'
 
@@ -57,8 +55,8 @@ gem 'bootsnap', require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
-  gem 'rspec-rails'
   gem 'rails-controller-testing'
+  gem 'rspec-rails'
 end
 
 group :development do
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "Using Users controller" do
-    it "gets users list" do
-      get "/users/"
+RSpec.describe 'Users', type: :request do
+  describe 'Using Users controller' do
+    it 'gets users list' do
+      get '/users/'
       expect(response).to have_http_status(:ok)
       expect(response).to render_template(:index)
       expect(response.body).to include('This is a list of all users')
     end
 
-    it "shows user info" do
-      get "/users/20"
+    it 'shows user info' do
+      get '/users/20'
       expect(response).to have_http_status(:ok)
       expect(response).to render_template(:show)
       expect(response.body).to include('Showing User info')
