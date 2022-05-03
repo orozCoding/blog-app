864f46dfb3c16905ea421e7d84a20e14f59c2524
orozcoding <orozcoding@gmail.com>
2022-05-03 00:05:27 -0400

    Add methods to post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+
+  def increment_posts_counter
+    user = User.find(user_id)
+    user.update(posts_counter: user.posts_counter + 1)
+  end
+
+  def recent_comments
+    Comment.where(post_id: id).order(created_at: :desc).first(5)
+  end
 end
