fcae1ca6f470ee69fd8e670928430aca05156d24
orozcoding <orozcoding@gmail.com>
2022-05-03 00:09:30 -0400

    Add method to like.rb model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  def increment_posts_counter
+  def increment_comments_counter
     post = Post.find(post_id)
     post.update(comments_counter: post.comments_counter + 1)
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def increment_likes_counter
+    post = Post.find(post_id)
+    post.update(likes_counter: post.likes_counter + 1)
+  end
 end
