cec841fe7e4bb7b2760504c98bf2a7ab014f9de0
orozcoding <orozcoding@gmail.com>
2022-05-06 00:16:55 -0400

    Refactor controllers to use flashes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,15 @@
 class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @like = @post.likes.create(author: current_user, post: @post)
-    redirect_to user_post_path(User.find(params[:user_id]), @post)
+    @user = User.find(params[:user_id])
+    @like = Like.new(author: current_user, post: @post)
+
+    respond_to do |format|
+      if @like.save
+        format.html { redirect_to user_post_path(@user, @post), success: 'You liked the post!' }
+      else
+        format.html { redirect_to user_post_path(@user, @post), danger: 'Something went wrong!' }
+      end
+    end
   end
 end
