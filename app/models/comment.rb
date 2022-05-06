9a8bc21809b1f522b00a004c75ef42e02c64c9da
orozcoding <orozcoding@gmail.com>
2022-05-06 00:02:18 -0400

    Add validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  validates :text, presence: true
+
   def increment_comments_counter
     post.increment!(:comments_counter)
   end
