82419a29e2e6a4dc2a8352bec3c1d5212c6be688
orozcoding <orozcoding@gmail.com>
2022-05-09 09:20:24 -0400

    Edit aplication controller to match new devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::Base
   # def current_user
   #   User.first
   # end
+
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :username, :email, :password)}
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:username, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :username, :email, :password, :current_password)}
+  end
 end
