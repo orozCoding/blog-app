ae8eff43ce77304ef79b067e58b32b38926582c6
orozcoding <orozcoding@gmail.com>
2022-05-09 08:54:55 -0400

    Comment out current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   add_flash_types :danger, :info, :warning, :success, :messages
 
-  def current_user
-    User.first
-  end
+  # def current_user
+  #   User.first
+  # end
 end
