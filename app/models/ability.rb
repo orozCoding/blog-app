a0c655e2ff36425d2fe1e0e9dd5e1d740c1da4da
unknown <mugishasamuel42@gmail.com>
2022-05-10 08:01:37 -0700

    Add abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,5 +3,18 @@ class Ability
 
   def initialize(user)
   
+    user ||= User.new
+
+    can :read, Post
+    can :read, Comment
+
+    return unless user.present?
+
+    can :manage, Post, author_id: user.id
+    can :manage, Comment, author_id: user.id
+
+    return unless user.admin?
+
+    can :manage, :all
   end
 end
