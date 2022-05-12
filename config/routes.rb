f49fb14d7542c793657c9647b0d77d8c83c28c7f
orozcoding <orozcoding@gmail.com>
2022-05-12 13:11:05 -0400

    Added api routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'users#index'
+
+  #api
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+      resources :users, only: %i[index show] do
+        resources :posts, only: %i[index] do
+          resources :comments, only: %i[index create] do
+          end
+        end
+      end
+    end
+  end
 end
